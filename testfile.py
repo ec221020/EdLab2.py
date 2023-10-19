5f9a98225627f38bdc05d09dfd45d00e9dd7c60a
ec221020 <ec221020@qmul.ac.uk>
2023-10-19 09:43:20 +0100

    new

--- testfile.py
+++ testfile.py
@@ -2,7 +2,6 @@ import unittest
 from funct import myfunct
 class test(unittest.TestCase):
     def test1(self):
-        self.assertEqual(myfunct(),1)
+        self.assertEqual(myfunct(),0)
 if __name__ == "__main__":
     unittest.main()
-    
